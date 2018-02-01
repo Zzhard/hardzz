60dc9c02798e0685869a6cec9fc0acc8f0ff0372
1178145190 <1178145190@qq.com>
2018-02-02 01:23:21 +0800

    jisuanyouwenti

--- calculator.py
+++ calculator.py
@@ -3,38 +3,40 @@ import sys
 
 
 def calculator(salary):
+	if salary<3500:
+		pass
+	else:
+		a=salary-(salary*0.165)-3500
+		if a<=1500:
+			b=salary-salary*0.165-a*0.03
+			print(b)
+			c=format(b,'0.2f')
+		elif a>1500 and a<=4500:
+			b=salary-salary*0.165-(a*0.1-105)
+			c=format(b,'0.2f')
+		elif a>4500 and a<=9000:
+			b=salary-salary*0.165-(a*0.2-555)
+			c=format(b,'0.2f')
+		elif a>9000 and a<=35000:
+			b=salary-salary*0.165-(a*0.25-1005)
+			c=format(b,'0.2f')
+		elif a>35000 and a<=55000:
+			b=salary-salary*0.165-(a*0.3-2755)
+			c=format(b,'0.2f')
+		elif a>55000 and a<=80000:
+			b=salary-salary*0.165-(a*0.35-5505)
+			c=format(b,'0.2f')
+		elif a>80000:
+			b=salary-salary*0.165-(a*0.45-13505)
+			c=format(b,'0.2f')
+		return c
 
-	a=salary-3500-(salary*0.165)
-	if a<=1500:
-		b=salary-salary*0.165-a*0.03
-		c=format(b,'0.2f')
-	elif a>1500 and a<=4500:
-		b=salary-salary*0.165-a*0.1-105
-		c=format(b,'0.2f')
-	elif a>4500 and a<=9000:
-		b=salary-salary*0.165-a*0.2-555
-		c=format(b,'0.2f')
-	elif a>9000 and a<=35000:
-		b=salary-salary*0.165-a*0.25-1005
-		c=format(b,'0.2f')
-	elif a>35000 and a<=55000:
-		b=salary-salary*0.165-a*0.3-2755
-		c=format(b,'0.2f')
-	elif a>55000 and a<=80000:
-		b=salary-salary*0.165-a*0.35-5505
-		c=format(b,'0.2f')
-	elif a>80000:
-		b=salary-salary*0.165-a*0.45-13505
-		c=format(b,'0.2f')
-	return c
-
-numberlist=[]
-salarylist=[]
+dic={}
+list=[]
 for arg in sys.argv[1:]:
 
 	
 	number,salary=arg.split(':')
-	numberlist.append(number)
 	try:
 		salary=int(salary)
 
@@ -43,8 +45,9 @@ for arg in sys.argv[1:]:
 
 	else:
 		finalsalary=calculator(salary)
-		salarylist.append(finalsalary)
+		dic[number]=finalsalary
+		list.append(number)
+for i in list:
+	print(i+':'+dic.get(i))
 
-dic=dict(zip(numberlist,salarylist))
-for key,value in dic.items():
-	print(key+":"+value)	
+
